556d298db3b507cb846098ff0645752745af77da
Kurisani-web <kurisanim2@gmail.com>
2025-05-25 22:22:55 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,19 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+// ✅ Import these
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App({ element }) {
   return (
     <>
       <Navbar />
       {element}
       <Footer />
+
+      {/* ✅ Add ToastContainer at the root */}
+      <ToastContainer position="top-right" autoClose={3000} />
     </>
   );
 }
